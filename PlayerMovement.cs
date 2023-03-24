ced6e15ea2708fdc4b5b7f227e2fdc1ff155f044
Ben Hughes <97689852+BenjaminHHughes@users.noreply.github.com>
2023-03-24 09:41:36 +0000

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed = 1f;
+    public float JumpForce = 1f;
+    public Animator animator;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (!Mathf.Approximately(0, movement))
+        {
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.001f)
+        {
+            rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+        animator.SetFloat("Speed", Mathf.Abs(movement));
+
+    }
+}
